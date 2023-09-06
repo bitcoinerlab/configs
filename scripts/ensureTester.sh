54ad92dbfe87fb06cf7ef4e2edbae2f530d6bb66
José Luis Landabaso Díaz <landabaso@gmail.com>
2023-09-06 10:13:47 +0200

    fixes: typo in package.json and in ensureTester.sh

--- scripts/ensureTester.sh
+++ scripts/ensureTester.sh
@@ -4,4 +4,4 @@
 # Introduces a 5-second sleep after starting the container for the first time to ensure consistent readiness.
 # The sleep ensures that, regardless of whether the user knows the initial state of the container (running or not),
 # the script always returns with the container in a similarly ready state.
-docker ps | grep bitcoinerlab/tester > /dev/null || (docker pull bitcoinerlab/tester && docker run -d -p 8080:8080 -p 60401:60401 -p 3002:3002 bitcoinerlab/tester &&
+docker ps | grep bitcoinerlab/tester > /dev/null || (docker pull bitcoinerlab/tester && docker run -d -p 8080:8080 -p 60401:60401 -p 3002:3002 bitcoinerlab/tester && sleep 5)
