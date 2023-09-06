7fe9db86be15298ef648331a76c5793ea2e69353
José Luis Landabaso Díaz <landabaso@gmail.com>
2023-09-06 10:09:55 +0200

    scripts: new ensureTester.sh script

---
+++ scripts/ensureTester.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Checks if the 'tester' container is running.
+# If not, pulls the latest 'bitcoinerlab/tester' image and starts a new container.
+# Introduces a 5-second sleep after starting the container for the first time to ensure consistent readiness.
+# The sleep ensures that, regardless of whether the user knows the initial state of the container (running or not),
+# the script always returns with the container in a similarly ready state.
+docker ps | grep bitcoinerlab/tester > /dev/null || (docker pull bitcoinerlab/tester && docker run -d -p 8080:8080 -p 60401:60401 -p 3002:3002 bitcoinerlab/tester &&
--- scripts/tester.sh
+++ scripts/tester.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-docker images | grep -q bitcoinerlab/tester || docker pull bitcoinerlab/tester; docker rm -f $(docker ps -a -q -f ancestor=bitcoinerlab/tester) > /dev/null 2>&1; docker run -d -p 8080:8080 -p 60401:60401 -p 3002:3002 bitcoinerlab/tester
-
+# Ensures a fresh instance of the 'tester' container is always running.
+# If the 'bitcoinerlab/tester' image is not found locally, it pulls the latest image.
+# Any existing 'tester' containers are removed before starting a new one for a clean slate.
+# Caution: The user must wait for the container to be in a ready state before using, as no sleep/delay is added in this script.
+docker images | grep -q bitcoinerlab/tester || docker pull bitcoinerlab/tester; 
+docker rm -f $(docker ps -a -q -f ancestor=bitcoinerlab/tester) > /dev/null 2>&1; 
+docker run -d -p 8080:8080 -p 60401:60401 -p 3002:3002 bitcoinerlab/tester
